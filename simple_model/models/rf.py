e93cfaddb2cd60dc755a15a1ab8f7a2f4bce68dc
Andrew Nichol <randrewnichol@berkeley.edu>
2018-04-27 22:45:27 -0700

    tuning model

--- simple_model/models/rf.py
+++ simple_model/models/rf.py
@@ -15,18 +15,34 @@ for df in [negative, positive]:
 							df["newsapi_totalResults"][i] / df["newsapi_rawResults"][i] \
 								for i in range(df.shape[0])]
 
+# for practical purposes
+positive = positive[positive["newsapi_rawResults"]!=0]
+
 negative["class"] = 0
 positive["class"] = 1
 
 full = negative.append(positive, ignore_index = True)
+# for col in ["num_orgs","num_non_orgs","num_found","num_titlecase","num_industries"]:
+# 	full[col]= (full[col] / full["num_articles"]) 
+
+# full = full.fillna(0)
+
+# cols = full.columns.tolist()
+# cols.remove("class")
+# full = full[cols + ["class",]]
 
 X = full.iloc[:,2:-1]
 y = full.iloc[:,-1]
 
-# X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=.2, random_state=1231)
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=.2, random_state=131)
 
 random_forest = RandomForestClassifier(n_estimators=1000)
-random_forest.fit(X, y)
+random_forest.fit(X_train, y_train)
+p = np.array([e[1] for e in random_forest.predict_proba(X_test)])
+
+# classes = np.greater_equal(p,.45).astype(int)
+
+# print(metrics.confusion_matrix(y_test, classes))
 
 # cuts = [[],[]]
 # pss = [sorted(np.array([e[1] for e in random_forest.predict_proba(X[full["class"]==0])])),
@@ -44,8 +60,8 @@ random_forest.fit(X, y)
 # print(X.iloc[1:2,:])
 # print(random_forest.predict_proba(X.iloc[1:2,:]))
 
-with open('rf.pkl', 'wb') as outfile:
-	_pickle.dump(random_forest, outfile)
+# with open('rf.pkl', 'wb') as outfile:
+# 	_pickle.dump(random_forest, outfile)
 
 
 
