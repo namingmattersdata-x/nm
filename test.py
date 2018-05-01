4ccb2a96b9ec8c0dcfcc977a9d2cc0e01eafcda9
Andrew Nichol <bigboiadmin@Andrews-MacBook-Pro.local>
2018-05-01 00:55:31 -0700

    deelete teste

--- test.py
+++
@@ -1,8 +0,0 @@
-from newsapi import NewsApiClient
-newsapi = NewsApiClient(api_key='25d399904da54cc3a0cec6eafbe8717b')
-all_articles = newsapi.get_everything(q="this is a test",
-                                      language='en', # necessary to only maintain english? probably for now
-                                      sort_by='relevancy',
-                                      page=1)
-from pprint import PrettyPrinter
-PrettyPrinter().pprint(all_articles)
